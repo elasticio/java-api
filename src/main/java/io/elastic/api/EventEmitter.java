8b53284e257b5be817a36ba26bb864b325db717d
Igor Drobiazko <igor.drobiazko@gmail.com>
2014-07-31 10:12:18 +0200

    Callback is now inner interface inside Emitter

--- src/main/java/io/elastic/api/EventEmitter.java
+++ src/main/java/io/elastic/api/EventEmitter.java
@@ -33,4 +33,19 @@ public final class EventEmitter {
         return this;
     }
 
+    /**
+     * This interface defines a callback to be used by {@link Executor}
+     * to pass errors, data and snapshots to its callee.
+     *
+     */
+    public interface Callback {
+
+        /**
+         * Invoked by {@link Executor} to pass errors, data and snapshots asynchronously.
+         *
+         * @param data data to be passed
+         */
+        void receive(Object data);
+    }
+
 }
