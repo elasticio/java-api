9830db6c5a4b8f1de60cdbf0244da252d09324ed
Igor Drobiazko <igor.drobiazko@gmail.com>
2014-07-31 09:54:38 +0200

    Added Builder for ExecutionParameters

--- src/main/java/io/elastic/api/ExecutionParameters.java
+++ src/main/java/io/elastic/api/ExecutionParameters.java
@@ -9,7 +9,7 @@ public final class ExecutionParameters {
     private final JsonObject configuration;
     private final JsonElement snapshot;
 
-    public ExecutionParameters(Message message, JsonObject configuration, JsonElement snapshot) {
+    private ExecutionParameters(Message message, JsonObject configuration, JsonElement snapshot) {
         this.message = message;
         this.configuration = configuration;
         this.snapshot = snapshot;
@@ -25,5 +25,46 @@ public final class ExecutionParameters {
 
     public JsonElement getSnapshot() {
         return snapshot;
+    }
+
+    public static final class Builder {
+        private final Message message;
+        private JsonObject configuration;
+        private JsonElement snapshot;
+
+
+        public Builder(Message message) {
+            if (message == null) {
+                throw new IllegalArgumentException("Message is required");
+            }
+
+            this.message = message;
+            this.configuration = new JsonObject();
+            this.snapshot = new JsonObject();
+        }
+
+        public Builder configuration(JsonObject configuration) {
+            this.configuration = configuration;
+
+            return this;
+        }
+
+        public Builder snapshot(JsonElement snapshot) {
+            this.snapshot = snapshot;
+
+            return this;
+        }
+
+        public ExecutionParameters build() {
+            if (this.configuration == null) {
+                throw new IllegalStateException("Configuration may not be null");
+            }
+
+            if (this.snapshot == null) {
+                throw new IllegalStateException("Snapshot may not be null");
+            }
+
+            return new ExecutionParameters(message, configuration, snapshot);
+        }
     }
 }
