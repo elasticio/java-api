b64b8f7947b7bddd901fae3384ca3543d2ad6e2e
Nenad <nenad@elastic.io>
2015-02-18 16:45:50 +0100

    Fix comment

--- src/main/java/io/elastic/api/SelectModelProvider.java
+++ src/main/java/io/elastic/api/SelectModelProvider.java
@@ -16,7 +16,7 @@ public interface SelectModelProvider {
      * Allows a component to dynamically populate select boxes
      * 
      * @param configuration Config data needed to execute the method
-     * @param identifier If the component has multiple select boxes to be populated the identifier helps determine which one requested data
+     * 
      * @return the select model for a particular select box
      */
     JsonObject getSelectModel(JsonObject configuration);
