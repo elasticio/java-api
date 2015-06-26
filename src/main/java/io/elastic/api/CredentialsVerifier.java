a733a39f5b94571acfbbd487d4f67d69c1f5b658
Igor Drobiazko <igor@elastic.io>
2015-06-26 09:30:43 +0200

    Added CredentialsVerifier interface

---
+++ src/main/java/io/elastic/api/CredentialsVerifier.java
@@ -0,0 +1,28 @@
+package io.elastic.api;
+
+import com.google.gson.JsonObject;
+
+/**
+ * Interface to be implemented by components to verify user's
+ * credentials before they are persisted.
+ * <p/>
+ * Typically a credentials verification is implemented by sending
+ * an arbitrary request to the given service and analyse the response.
+ * For example if the response's status code is <i>HTTP 401 Unauthorized</i>
+ * the verification failed.
+ */
+public interface CredentialsVerifier {
+
+    /**
+     * Performs the verification credentials in the given <i>configuration</i>.
+     * The <i>configuration</i> parameter is a JSON object containing either
+     * OAuth access token, API key or username/password combination provided
+     * by a user during authentication process. These values are to be verified
+     * by sending an arbitrary request to the desired service.
+     *
+     * @param configuration contains OAuth access token, API key,
+     *                      username/password to be verified
+     * @throws InvalidCredentialsException if the credentials are invalid
+     */
+    void verify(JsonObject configuration) throws InvalidCredentialsException;
+}
---
+++ src/main/java/io/elastic/api/InvalidCredentialsException.java
@@ -0,0 +1,34 @@
+package io.elastic.api;
+
+/**
+ * Thrown by {@link CredentialsVerifier#verify(com.google.gson.JsonObject)}
+ * when authentication credentials are invalid.
+ */
+public class InvalidCredentialsException extends Exception {
+
+    /**
+     * Creates a new InvalidCredentialsException with a null detail message.
+     */
+    public InvalidCredentialsException() {
+    }
+
+    /**
+     * Creates a new InvalidCredentialsException with the specified message.
+     *
+     * @param message message the exception detail message
+     */
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+
+    /**
+     * Creates a new InvalidCredentialsException with the specified
+     * detail message and cause.
+     *
+     * @param message   the exception detail message
+     * @param throwable the Throwable that caused this exception
+     */
+    public InvalidCredentialsException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
