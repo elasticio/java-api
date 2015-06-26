bfc43742297c28bf300639033a9e658e745cca1b
Igor Drobiazko <igor@elastic.io>
2015-06-26 09:30:23 +0200

    Improved docs

--- src/main/java/io/elastic/api/SelectModelProvider.java
+++ src/main/java/io/elastic/api/SelectModelProvider.java
@@ -4,19 +4,27 @@ import com.google.gson.JsonObject;
 
 
 /**
- * 
  * Interface to be implemented by components which want to provide select model.
- * 
- * @author Nenad Nikolic nenad@elastic.io
- *
  */
 public interface SelectModelProvider {
-	
-	 /**
-     * Allows a component to dynamically populate select boxes
-     * 
+
+    /**
+     * Allows a component to dynamically populate select boxes.
+     * The given <i>configuration</i> object contains authentication data
+     * that might be required if the values are retrieved from the desired service.
+     * The method must return a simple JSON object in which the keys are mapped
+     * to human readable labels, as shown in the following example.
+     * <pre>
+     * <code>
+     *
+     * {
+     *     "de" : "Germany"
+     *     "us" : "United States"
+     * }
+     * </code>
+     * </pre>
+     *
      * @param configuration Config data needed to execute the method
-     * 
      * @return the select model for a particular select box
      */
     JsonObject getSelectModel(JsonObject configuration);
